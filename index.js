6dde188658f603c6fa734f77af37aa732ea2551b
vinodh <vinodh2019@gmail.com>
2019-04-16 23:29:16 -0400

    added albumlist

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // import a library to create a component
 import React from "react";
 import Header from "./src/components/header";
-import { Text, AppRegistry } from "react-native";
+import { View, AppRegistry } from "react-native";
+import AlbumList from "./src/components/AlbumList";
+
 // create a component
-const App = () => {
-  return <Header />;
-};
+const App = () => (
+  <View>
+    <Header headerText={"albums!"} />
+    <AlbumList />
+  </View>
+);
 
 // render it to device
 AppRegistry.registerComponent("albums", () => App);
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
